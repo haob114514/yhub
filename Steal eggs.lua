3843d13c7c982c1c008388ff91e271e2f429e841
haob114514 <2484064926@qq.com>
2026-08-29 12:06:26 +0800

    Update Steal eggs.lua

--- Steal eggs.lua
+++ Steal eggs.lua
@@ -1,7 +1,7 @@
 local main = loadstring(game:HttpGet("https://raw.githubusercontent.com/Wlzhmaa/UWU/refs/heads/main/Chinese%20translation"))
 main()
 
-task.wait(6) 
+task.wait(8) 
 
 local translate = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ryuun0x/Clover/main/main.lua"))
 translate()

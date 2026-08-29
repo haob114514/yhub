bc30c4d206c5aa04f07621c8d411c6222f370e77
haob114514 <2484064926@qq.com>
2026-08-29 11:58:21 +0800

    Add files via upload

---
+++ Steal eggs.lua
@@ -0,0 +1,7 @@
+local main = loadstring(game:HttpGet("https://raw.githubusercontent.com/Wlzhmaa/UWU/refs/heads/main/Chinese%20translation"))
+main()
+
+task.wait(6) 
+
+local translate = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ryuun0x/Clover/main/main.lua"))
+translate()
